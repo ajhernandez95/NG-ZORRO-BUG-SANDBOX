6b97ffc0954a88167cfc171f920cac7dac98fa68
Alex Hernandez <alexhernandez@engage2learn.org>
2020-12-14 09:53:06 -0600

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  time: any;
+export class AppComponent implements OnInit {
+  timeOne: any;
+  timeTwo: any = new Date(1607960844 * 1000);
 
-  timeChanged() {
-    console.log(this.time);
+  ngOnInit() {
+    console.log(this.timeOne, this.timeTwo)
+  }
+
+  timeChangedOne() {
+    console.log(this.timeOne);
+  }
+
+  timeChangedTwo() {
+    console.log(this.timeTwo)
   }
 }
